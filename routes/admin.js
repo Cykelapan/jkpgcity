aa2bacb408261d7bf96db451d252b2eb4c7e6f05
Cykelapan <147327743+Cykelapan@users.noreply.github.com>
2024-03-17 23:46:22 +0100

    merged

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,7 @@ const router = express.Router();
 //NEEDS TO BE ADMIN, create new stores, update and remove
 //WHERE there is comments remove or add
 
+
 router.route('/')
     .get(async (req, res,) => {
         const db = req.db;
