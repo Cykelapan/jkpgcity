248675bceb185553319411b1fbf03b58a4aaf040
Cykelapan <147327743+Cykelapan@users.noreply.github.com>
2024-03-20 04:36:30 +0100

    Update login.js
    .

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-
 const validateRegister = require('../backend/validators/register');
 const validateLogin = require('../backend/validators/login');
 const jwt = require('../backend/auth/createToken');
@@ -12,33 +11,33 @@ router.use(bodyParser.json());
 
 router.route('/')
   .get(async (req, res,) => {
-    res.status(200).send('<h1> Login PAGE </h1>');
+    res.status(200).json({});
   })
 
   .post(async (req, res) => {
     // handle validation here
     const validate = await validateLogin(req.body);
     if (validate.haveErrors) {
-      return res.status(400).redirect('/login');
+      return res.status(400).json({})
     }
-    
+
     const user = await db.getUserLogin(
       validate.validData.username,
       validate.validData.password
     );
-    
+
     //user dose not exist
     if (!user) {
-      return res.status(500).redirect('/login');
+      return res.status(500).json({ error: "Wrong password or username" })
     }
-    
+
     // create jwt token
     const token = await jwt.createToken(user);
     if (!token) {
-      res.status(500).redirect('/login');
+      res.status(500).json({ error: "Could not create token" })
       return;
     }
-    
+
     await db.addToken(token);
     // successfully identified
     res.setHeader('Authorization', `Bearer ${token}`).status(200).json({
@@ -48,6 +47,13 @@ router.route('/')
       description: `${user.username} is loggin`
     });
   });
+
+
+router.route('/register')
+  .get(async (req, res) => {
+    res.status(200).json({});
+  })
+
 
 
 router.route('/register')
@@ -65,7 +71,7 @@ router.route('/register')
     } else {
 
       const checkDB = await db.checkUsernameAndEmail(
-        validate.validData.username, 
+        validate.validData.username,
         validate.validData.email
       );
 
